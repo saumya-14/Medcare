fd7d1f691e630a605f9f2b5efa4b9dbd612b021a
saumya-14 <saumya140904@gmail.com>
2024-12-27 14:15:06 +0530

    done

--- src/app/api/fetchdoct/route.ts
+++ src/app/api/fetchdoct/route.ts
@@ -1,21 +1,18 @@
-import { NextRequest,NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { Doctor } from "@/models/usermodel";
 import { connect } from "@/dbConfig/db";
 
-
-export async function GET(req:NextRequest,res:NextResponse, { params }: { params: { id: string } }) {
-
+export async function GET(req: NextRequest) {
     if (req.method !== 'GET') {
-        return NextResponse.json({ message: 'Method Not Allowed' });
-      }
-      try{
-           await connect();
-           const doctor = await Doctor.find().exec();
-           return NextResponse.json(doctor);
+        return NextResponse.json({ message: 'Method Not Allowed' }, { status: 405 });
+    }
 
-      }catch(error:any){
-        console.log('Error fetching doctors:', error);
-        return NextResponse.json({ message: 'Error fetching doctors' });
-
-      }
+    try {
+        await connect();
+        const doctors = await Doctor.find().exec();
+        return NextResponse.json(doctors, { status: 200 });
+    } catch (error: any) {
+        console.error('Error fetching doctors:', error);
+        return NextResponse.json({ message: 'Error fetching doctors' }, { status: 500 });
+    }
 }
